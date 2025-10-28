035d5a0ffa848ebd16d82f3b7349565011f40023
alext-42 <techer.alexandre9@gmail.com>
2025-10-28 17:45:19 +0100

    Add files via upload

---
+++ Tower_Defense-test/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/4A/Tower_Defense-test/src/Astar.cpp" "CMakeFiles/main.dir/src/Astar.cpp.o" "gcc" "CMakeFiles/main.dir/src/Astar.cpp.o.d"
+  "/home/ubuntu/4A/Tower_Defense-test/src/enemy.cpp" "CMakeFiles/main.dir/src/enemy.cpp.o" "gcc" "CMakeFiles/main.dir/src/enemy.cpp.o.d"
+  "/home/ubuntu/4A/Tower_Defense-test/src/game.cpp" "CMakeFiles/main.dir/src/game.cpp.o" "gcc" "CMakeFiles/main.dir/src/game.cpp.o.d"
+  "/home/ubuntu/4A/Tower_Defense-test/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/home/ubuntu/4A/Tower_Defense-test/src/projectile.cpp" "CMakeFiles/main.dir/src/projectile.cpp.o" "gcc" "CMakeFiles/main.dir/src/projectile.cpp.o.d"
+  "/home/ubuntu/4A/Tower_Defense-test/src/render.cpp" "CMakeFiles/main.dir/src/render.cpp.o" "gcc" "CMakeFiles/main.dir/src/render.cpp.o.d"
+  "/home/ubuntu/4A/Tower_Defense-test/src/tower.cpp" "CMakeFiles/main.dir/src/tower.cpp.o" "gcc" "CMakeFiles/main.dir/src/tower.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
