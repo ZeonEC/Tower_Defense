4073cd39d39ef95e56bf001aba71037cf23f6adc
alext-42 <techer.alexandre9@gmail.com>
2025-11-02 17:25:57 +0100

    Delete Tower_Defense-test directory

--- Tower_Defense-test/build/CMakeFiles/main.dir/src/projectile.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/main.dir/src/projectile.cpp.o: \
- /home/ubuntu/4A/Tower_Defense-test/src/projectile.cpp \
- /usr/include/stdc-predef.h \
- /home/ubuntu/4A/Tower_Defense-test/include/projectile.hpp
